93e3b74c143ce116c2ce604ba11ac1faed23117a
mcqMonarch <monarch.mcq@qmail.com>
2024-03-30 15:22:52 +0300

    try to make something that works

--- less10/t2.py
+++ less10/t2.py
@@ -1,16 +1,22 @@
 def desk(x, y):
     global platform
     global y1
+    global b
     y1 = y
     platform = [1 for _ in range(x)]
     print(platform)
+    b = ((1, 1), (2, 1))
 
 
 def f():
-    for _ in range(1, y1):
+    for i in range(1, y1):
         for j in range(1, len(platform)):
             platform[j] += platform[j - 1]
+            if (j, i) in b:
+                platform[j] = 0
+            print(platform)
     print(platform[-1])
+
 
 desk(5, 3)
 f()
